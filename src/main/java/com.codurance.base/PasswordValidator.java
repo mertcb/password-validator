e0c520e3d437b7792d8d7b146b7ab298f3febeee
Mert Bilgiç <mert.bilgic@trendyol.com>
2022-08-02 15:17:29 +0300

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/main/java/com.codurance.base/PasswordValidator.java

--- src/main/java/com.codurance.base/PasswordValidator.java
+++ src/main/java/com.codurance.base/PasswordValidator.java
@@ -2,6 +2,29 @@ package com.codurance.base;
 
 public class PasswordValidator {
     public boolean validate(String password) {
-        return false;
+        if(!isLongerThanEightCharacters(password)){
+            return false;
+        }
+
+        if(!isContainUppercase(password)){
+            return false;
+        }
+
+        if(!isContainLowercase(password)){
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean isLongerThanEightCharacters(String password){
+        return password.length()>8;
+    }
+    private boolean isContainUppercase(String password){
+        return !password.matches(".*[A-Z].*");
+    }
+
+    private boolean isContainLowercase(String password){
+        return !password.matches(".*[a-z].*");
     }
 }
--- src/test/java/com/codurance/base/PasswordValidatorShould.java
+++ src/test/java/com/codurance/base/PasswordValidatorShould.java
@@ -2,15 +2,32 @@ package com.codurance.base;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PasswordValidatorShould {
 
     @Test
-    void validateWhenMoreThan8Characters() {
+    void notValidateWhenLessThan8Characters() {
         PasswordValidator validator = new PasswordValidator();
-        String password = "123456789";
+        String password = "1234567";
         boolean result = validator.validate(password);
-        assertTrue(result);
+        assertFalse(result);
+    }
+
+    @Test
+    void notValidateWhenPasswordHasCapitalCharacter() {
+        PasswordValidator validator = new PasswordValidator();
+        String password = "123456789g";
+        boolean result = validator.validate(password);
+        assertFalse(result);
+    }
+
+    @Test
+    void notValidateWhenPasswordHasNotAnyLowercaseLetter() {
+        PasswordValidator validator = new PasswordValidator();
+        String password = "123456789Gg";
+        boolean result = validator.validate(password);
+        assertFalse(result);
     }
 }
